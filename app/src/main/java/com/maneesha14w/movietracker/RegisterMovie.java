b5f82f0dd6d74bc9e3b3cd938e367d732e4d7a97
Maneesha <maneesha14w@gmail.com>
2021-04-09 21:57:14 +0530

    ui for register movie page done.

--- app/src/main/java/com/maneesha14w/movietracker/RegisterMovie.java
+++ app/src/main/java/com/maneesha14w/movietracker/RegisterMovie.java
@@ -3,13 +3,47 @@ package com.maneesha14w.movietracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class RegisterMovie extends AppCompatActivity {
+public class RegisterMovie extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private Spinner ratingSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_movie);
+
+        ratingSpinner = findViewById(R.id.spin_rating);
+        ratingSpinner.setOnItemSelectedListener(this);
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.rating_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        ratingSpinner.setAdapter(adapter);
+        //ratingSpinner.getSelectedItem().toString();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        try {
+            int rating = (Integer) parent.getItemAtPosition(position);
+        }
+        catch (Exception e) {
+            Toast.makeText(this, "Please Select a proper Rating!", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        Toast.makeText(this, "Nothing Selected", Toast.LENGTH_SHORT).show();
     }
 
 }
